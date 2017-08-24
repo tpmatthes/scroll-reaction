dd7c0a2d8face276565b0a6e004531fe4406d3e1
Tim-Patrick Matthes <tpmatthes@users.noreply.github.com>
2017-08-24 08:59:34 +0200

    Added missing return statement to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,14 @@ var minifyJS = require('gulp-minify');
 
 // Minify all JavaScript files
 gulp.task('buildJS', function() {
-	gulp.src('src/*.js')
-		.pipe(minifyJS({
-			ext: {src: '.js', min: '.min.js'}, // File extensions
-			noSource: true, // Don't copy the unminified source code
-			mangle: true, // Shorten variable names
-			preserveComments: 'some' // Preserve comments like /*! */
-		}))
-		.pipe(gulp.dest('build'));
+	return gulp.src('src/*.js')
+	.pipe(minifyJS({
+		ext: {src: '.js', min: '.min.js'}, // File extensions
+		noSource: true, // Don't copy the unminified source code
+		mangle: true, // Shorten variable names
+		preserveComments: 'some' // Preserve comments like /*! */
+	}))
+	.pipe(gulp.dest('build'));
 });
 
 // The command 'gulp' builds everything
