7fcbda78febbb38d6e02747e05d675891e8a153c
Tim-Patrick Matthes <tpmatthes@users.noreply.github.com>
2018-01-14 11:18:35 +0100

    Prepared 1.2.0 release
    Not enough changes for version 2.0

--- src/scroll-reaction.js
+++ src/scroll-reaction.js
@@ -1,5 +1,5 @@
 /*!
- * ScrollReaction 2.0.0 by Tim-Patrick Matthes
+ * ScrollReaction 1.2.0 by Tim-Patrick Matthes
  * Performant, dependency free and fully customizable scroll effects
  * Learn more: https://github.com/tpmatthes/scroll-reaction
  * MIT License
